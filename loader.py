0cd193d9352c617c500250ae7da7e07678a74373
JITHUKNR <96571770+JITHUKNR@users.noreply.github.com>
2026-07-02 06:59:13 +0530

    Initialize loader.py for TrendaMoviesBot

---
+++ loader.py
@@ -0,0 +1,6 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+# ഇവിടെയാണ് app റെഡിയാകുന്നത്
+app = Client("TrendaMoviesBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
